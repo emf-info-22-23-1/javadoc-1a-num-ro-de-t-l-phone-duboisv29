ce0d3f35e5b2f71c96b5569b5963c2fc2c18893e
duboisv <duboisv@WSTEMFA44-12.stu.net.fr.ch>
2024-04-17 09:03:33 +0200

    Fini ecercice 3B JavaDoc

--- src/ch/emf/atelierpo/wrk/Worker.java
+++ src/ch/emf/atelierpo/wrk/Worker.java
@@ -3,11 +3,20 @@ package ch.emf.atelierpo.wrk;
 
 /**
  *
- * @author Mario Ramalho(EMF - Fribourg)
+ * @author Valentin Dubois(EMF - Fribourg)
  */
 public class Worker {
 
-
+ /**
+     * Cette méthode tente de controler les numeros de téléphone mobile.
+     *
+     * @param prefix le prfix du numero de telephone, on en utilise que
+     * 4 : +4177, +4171, +4176, 079
+     * @param numero le numero de telephone mobile de la personne. Les
+     * seuls caracteres autorise sont les chiffres de 0 a 9 et les espaces
+     * @return vrai ou faux si le controle a bien ete fait ou non.
+     */ /**
+**/
     public boolean controleNumeroTelMobile(String prefix, String numero) {
         return false;
     }
